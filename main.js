0db252a1fb24184ac2dfb4f7eb644a4e2583140d
nathasjafink <nathasja.nielsen@gmail.com>
2023-10-04 11:42:48 +0200

    Changes to 2 and new function

--- main.js
+++ main.js
@@ -15,15 +15,55 @@ function clothing(temperature) {
 }
 console.log(clothing(10));
 
-// 2 - Dice game
+// 2 - Dice game part 1 & part 2
 function simulateDiceRoll(numRolls) {
+    let jackpot = 0
     for (let i = 0; i < numRolls; i++) {
         const rollResult = Math.floor(Math.random() * 6) + 1;
         if (rollResult === 6) {
             console.log("You just hit 6!");
+            jackpot += 1
+        }
+        if (jackpot === numRolls) {
+            console.log("Jackpot 🎉");
         }
         console.log(`Roll ${i+1}: ${rollResult}`);
     }
 }
 
-simulateDiceRoll(4);
+simulateDiceRoll(1);
+
+// 3 - Build a sentiment analyzer
+function sentimentAnalyzer(sentence) {
+    const postiveWords = ["happy","joyful","nice","awesome","super"];
+    const negativeWords =  ["hate","sad","bothered","sad","bad"];
+    const words = sentence.toLowerCase().split(" ");
+    let score = 0;
+    const foundPostiveWords = [];
+    const foundNegativeWords = [];
+    words.forEach((word) => {
+        if (postiveWords.includes(word)) {
+            score += 1;
+            foundPostiveWords.push(word);
+        } else if (negativeWords.includes(word)) {
+            score -= 1;
+            foundNegativeWords.push(word);
+        }
+    });
+       const result = {
+           score: score,
+           postiveWords: foundPostiveWords,
+           negativeWords: foundNegativeWords
+       };
+       return result;
+
+}
+const sentence1 = "I am mega super happy.";
+const sentence2 = "I was really bothered today and i really hate this day."
+
+const result1 = sentimentAnalyzer(sentence1);
+const result2 = sentimentAnalyzer(sentence2);
+
+console.log(result1);
+console.log(result2);
+
